724dd50b65ea2e9bf063846fa0c0512f03064ace
John Halmaghi <john.g.halmaghi@gmail.com>
2017-07-08 21:56:07 -0700

    Added the selection of media types from Main Activity

--- Streamline/app/src/main/java/com/example/john/streamline/MainActivity.java
+++ Streamline/app/src/main/java/com/example/john/streamline/MainActivity.java
@@ -4,20 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    HashMap<Button, Boolean> mediaTypeSelections = new HashMap<Button, Boolean>();
+    Button tvShows;
+    Button movies;
+    Button both;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        tvShows = (Button) findViewById(R.id.button_tv);
+        tvShows.setOnClickListener(this);
+        movies = (Button) findViewById(R.id.button_movies);
+        movies.setOnClickListener(this);
+        both = (Button) findViewById(R.id.button_both);
+        both.setOnClickListener(this);
+        mediaTypeSelections.put(tvShows, false);
+        mediaTypeSelections.put(movies, false);
+        mediaTypeSelections.put(both, false);
     }
 
-    public void nextActivity(View view){
+    private boolean getSelectionState(Button mediaType) {
+        return mediaTypeSelections.get(mediaType);
+    }
+
+    private void switchSelectionState(Button mediaType) {
+        if (mediaTypeSelections.get(mediaType)) {
+            mediaTypeSelections.put(mediaType, false);
+            mediaType.setBackgroundColor(getResources().getColor(R.color.white));
+        } else {
+            mediaTypeSelections.put(mediaType, true);
+            mediaType.setBackgroundColor(getResources().getColor(R.color.selected));
+        }
+    }
+
+    public void buttonPress(Button pressedButton) {
+        switchSelectionState(pressedButton);
+    }
+
+
+    public void nextActivity(View view) {
         Intent intent = new Intent(MainActivity.this, Sources.class);
+        String Selections = mediaTypeSelections.toString();
+        intent.putExtra("Selections", Selections);
         startActivity(intent);
     }
 
+
+    @Override
+    public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button_tv:
+                    switchSelectionState(tvShows);
+                    break;
+                case R.id.button_movies:
+                    switchSelectionState(movies);
+                    break;
+                case R.id.button_both:
+                    switchSelectionState(both);
+                    break;
+            }
+    }
 }
--- Streamline/app/src/main/java/com/example/john/streamline/Sources.java
+++ Streamline/app/src/main/java/com/example/john/streamline/Sources.java
@@ -3,7 +3,12 @@ package com.example.john.streamline;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class Sources extends AppCompatActivity {
 
@@ -11,8 +16,30 @@ public class Sources extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
-    }
+        Intent intent = getIntent();
+        String selections = (String)intent.getSerializableExtra("Selections");
+        HashMap<String, String> mediaTypeSelections = convertToStringToHashMap(selections);
 
+        Boolean tvShows = getBoolValues(mediaTypeSelections.get("app:id/button_tv"));
+        Boolean movies = getBoolValues(mediaTypeSelections.get("app:id/button_movies"));
+        Boolean both = getBoolValues(mediaTypeSelections.get("app:id/button_both"));
+        Log.v("tv: ", tvShows.toString());
+        Log.v("movies: ", movies.toString());
+        Log.v("both: ", both.toString());
+    }
+    protected HashMap<String,String> convertToStringToHashMap(String text){ //from https://www.tomred.net/java/hashmap-tostring-and-back-to-hashmap.html
+        HashMap<String,String> data = new HashMap<String,String>();
+        Pattern p = Pattern.compile("[\\{\\}\\=\\, ]++");
+        String[] split = p.split(text);
+        for ( int i=1; i+2 <= split.length; i+=2 ){
+            data.put( split[i], split[i+1] );
+        }
+        return data;
+    }
+    private Boolean getBoolValues(String value){
+        if(value.equals("true")) return true;
+        else return false;
+    }
     public void nextActivity(View view){
         Intent intent = new Intent(Sources.this, Results.class);
         startActivity(intent);
